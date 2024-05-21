5a1368b6dfd15e845ddd78126f8f587846a0e072
jxjx359 <jonashofacker9@gmail.com>
2024-05-21 19:44:57 +0200

    Vererbung

--- OOP Beispiel/Program.cs
+++ OOP Beispiel/Program.cs
@@ -7,6 +7,27 @@ namespace OOP_Beispiel
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Car car = new Car()
+            {
+                Acceleration = 100,
+                DoorCount = 5,
+                Breakspeed = 50,
+                Speed = 200
+            };
+
+            car.LogInformation();
+
+            Plane plane = new Plane()
+            {
+                Acceleration = 500,
+                DoorCount = 10,
+                Breakspeed = 200,
+                Speed = 600,
+                HasJetDrive = true
+            };
+
+            plane.LogInformation();
         }
     }
 }
---
+++ OOP Beispiel/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Beispiel
+{
+    abstract class Vehicle
+    {
+        public float Speed { get; set; }
+        public float Acceleration { get; set; }
+        public float Breakspeed { get; set; }
+
+        public int DoorCount { get; set; }
+
+        public void LogInformation()
+        {
+            Console.WriteLine("Das Fahrzeug fährt mit " + Speed + " km/h.");
+        }
+    }
+}
